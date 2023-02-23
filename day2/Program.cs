148026639859cd0e8ca3d6471a9c2a43cbfb2f72
nikitaptashinskiy <ptashinskiy.n@mail.ru>
2023-02-23 10:31:23 +0300

    create z1 and z2

---
+++ day2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Write("Введите чило х: ");
+double x = Convert.ToDouble(Console.ReadLine());
+double y;
+if (x >= 1 && x <= 3)
+{
+    y = 2 * (x * x) - 3 * Math.Exp(Math.Sin(x));
+    Console.WriteLine($"y = {y}");
+}
+if (x>3)
+{
+    y = 3 * Math.Tan(x);
+    Console.WriteLine($"y = {y}");
+}
+Console.ReadLine();
---
+++ zd2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int chast = m % n;
+if (chast == 0)
+{
+    Console.WriteLine($"Частное от деления чисел {m}/{n} равно {chast}");
+}
+else
+{
+    Console.WriteLine("M на N нацело не делится");
+}
+Console.ReadLine();
