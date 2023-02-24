52a0018140b880ea145b92fadaf74f895b1d8600
ptashkos <ptashinskiy.n@mail.ru>
2023-02-24 19:06:09 +0300

    d1

--- day 1/zd2/Program.cs
+++ day 1/zd2/Program.cs
@@ -1,13 +1,4 @@
-﻿//Console.Write("Введите трехзначное число: ");
-//int number = Convert.ToInt32(Console.ReadLine());
-//int b;
-//b = number % 100;
-//number = 
-//number = b * 100 + number;
-//Console.WriteLine(number);
-//Console.ReadLine();
-
-int temp, k, x, y;
+﻿int temp, k, x, y;
 Console.WriteLine("Введите трехзначное число");
 k = Convert.ToInt32(Console.ReadLine());
 temp = k;
