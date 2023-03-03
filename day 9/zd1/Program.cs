9ac5af8addad45cee519c27f83fecd0c73e544b8
ptashkos <ptashinskiy.n@mail.ru>
2023-03-03 10:43:48 +0300

    day9

---
+++ day 9/zd1/Program.cs
@@ -0,0 +1,39 @@
+﻿AERROFLOT[] flights = new AERROFLOT[7];
+for (int i =0; i< flights.Length; i++)
+{
+    Console.WriteLine($"Введите данные для реейса номер {i+1}:");
+    Console.WriteLine($"Название пункта назначения:");
+    flights[i].target_point = Console.ReadLine();
+    Console.WriteLine($"Номер рейса ");
+    flights[i].flight_number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Тип самолета:");
+    flights[i].airplane_type = Console.ReadLine();
+}
+
+Array.Sort(flights, (f1, f2) => f1.flight_number.CompareTo(f2.flight_number));
+Console.Write("Введите название пункта назначения: ");
+string destination = Console.ReadLine();
+bool found = false;
+
+for (int i = 0; i < flights.Length; i++)
+{
+    if (flights[i].target_point == destination)
+    {
+        Console.WriteLine("Рейс №" + flights[i].flight_number + ", тип самолета: " + flights[i].airplane_type);
+        found = true;
+    }
+}
+
+if (!found)
+{
+    Console.WriteLine("Рейсы в пункт назначения " + destination + " не найдены.");
+}
+
+Console.ReadKey();
+
+struct AERROFLOT
+{
+    public string target_point;
+    public int flight_number;
+    public string airplane_type;
+}
---
+++ day 9/zd2/Program.cs
@@ -0,0 +1,53 @@
+﻿Accauntant accauntant = new Accauntant();
+
+Console.WriteLine("Введите отработанные часы для менеджера:");
+int managerHours = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите отработанные часы для программиста:");
+int programmerHours = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите отработанные часы для дизайнера:");
+int designerHours = int.Parse(Console.ReadLine());
+
+if (accauntant.AskForBonus(Post.Manager, managerHours))
+{
+    Console.WriteLine("Менеджер заслуживает премию.");
+}
+else
+{
+    Console.WriteLine("Менеджер не заслуживает премию.");
+}
+
+if (accauntant.AskForBonus(Post.Programmer, programmerHours))
+{
+    Console.WriteLine("Программист заслуживает премию.");
+}
+else
+{
+    Console.WriteLine("Программист не заслуживает премию.");
+}
+
+if (accauntant.AskForBonus(Post.Designer, designerHours))
+{
+    Console.WriteLine("Дизайнер заслуживает премию.");
+}
+else
+{
+    Console.WriteLine("Дизайнер не заслуживает премию.");
+}
+Console.ReadLine();
+
+enum Post
+{
+    Manager = 160,
+    Programmer = 120,
+    Designer = 140
+}
+
+class Accauntant
+{
+    public bool AskForBonus(Post worker, int hours)
+    {
+        return hours > (int)worker;
+    }
+}
